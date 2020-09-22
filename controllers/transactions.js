c0059bbb163f580aa6808afd1f81f3e12c07ffb0
Keith Dumont <keithdumont@Keiths-MacBook-Pro.local>
2020-09-21 18:15:22 -0700

    added mongodb, mongoose

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,17 +1,30 @@
+const Transaction = require('../models/Transaction');
 // Get all transactions
 // GET /api/v1/transactions
-exports.getTransactions = (req, res, next) => {
-  res.send('GET transactions');
+exports.getTransactions = async (req, res, next) => {
+  try {
+    const transactions = await Transaction.find();
+    return res.status(200).json({
+      success: true,
+      count: transactions.length,
+      data: transactions,
+    });
+  } catch (err) {
+    return res.send(500).json({
+      success: false,
+      error: 'Server Error',
+    });
+  }
 };
 
 // Add transactions
 // POST /api/v1/transactions
-exports.addTransaction = (req, res, next) => {
+exports.addTransaction = async (req, res, next) => {
   res.send('POST transaction');
 };
 
 // Delete transactions
 // DELETE /api/v1/transactions/:id
-exports.deleteTransaction = (req, res, next) => {
+exports.deleteTransaction = async (req, res, next) => {
   res.send('DELETE transaction');
 };
