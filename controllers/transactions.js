ee73eb6d7a97389b2d6d66560e2589a916d209a0
Keith Dumont <keithdumont@Keiths-MacBook-Pro.local>
2020-10-03 10:35:49 -0700

    backend completed

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -10,7 +10,7 @@ exports.getTransactions = async (req, res, next) => {
       data: transactions,
     });
   } catch (err) {
-    return res.send(500).json({
+    return res.status(500).json({
       success: false,
       error: 'Server Error',
     });
@@ -20,11 +20,51 @@ exports.getTransactions = async (req, res, next) => {
 // Add transactions
 // POST /api/v1/transactions
 exports.addTransaction = async (req, res, next) => {
-  res.send('POST transaction');
+  try {
+    const { text, amount } = req.body;
+
+    const transaction = await Transaction.create(req.body);
+
+    return res.status(201).json({
+      success: true,
+      data: transaction,
+    });
+  } catch (err) {
+    if (err.name === 'ValidationError') {
+      const messages = Object.values(err.errors).map((val) => val.message);
+      return res.status(400).json({
+        success: false,
+        error: messages,
+      });
+    } else {
+      return res.status(500).json({
+        success: false,
+        error: 'Server Error',
+      });
+    }
+  }
 };
 
 // Delete transactions
 // DELETE /api/v1/transactions/:id
 exports.deleteTransaction = async (req, res, next) => {
-  res.send('DELETE transaction');
+  try {
+    const transaction = await Transaction.findById(req.params.id);
+    if (!transaction) {
+      return res.status(404).json({
+        success: false,
+        error: 'No transaction found',
+      });
+    }
+    await transaction.remove();
+    return res.status(200).json({
+      success: true,
+      data: {},
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      error: 'Server Error',
+    });
+  }
 };
